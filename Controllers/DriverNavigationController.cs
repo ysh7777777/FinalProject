a09f28353c4afb1bbd91b8022cffd38467330f15
jjason4608 <jjason71311@gmail.com>
2026-08-21 16:39:40 +0800

    connect driver navigation to trip db

--- Controllers/DriverNavigationController.cs
+++ Controllers/DriverNavigationController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace finalProject.Controllers
 {
     public class DriverNavigationController : Controller
     {
+        private readonly RideHailingDbContext _context;
+
+        public DriverNavigationController(
+            RideHailingDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Navigation()
         {
-            return View();
+            var driverId = "D001";
+
+            var trip = _context.Trips
+                .Where(t => t.AssignedDriverId == driverId)
+                .OrderBy(t => t.DepartureTime)
+                .FirstOrDefault();
+
+            return View(trip);
         }
     }
 }
