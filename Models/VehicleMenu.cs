81a0b9c97dfe17674bce19fc46f3db3d63d01d28
lance0913 <dickksss12341@gmail.com>
2026-08-23 22:51:35 +0800

    將新的資料表(VehicleMenu)建立Model。將新資料表(VehicleMenu)該增加的東西增加到DbContext中。

---
+++ Models/VehicleMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProject.Models;
+
+// 因為新增 VehicleMenu 表單，所以新增 (08/23 益)
+public partial class VehicleMenu
+{
+    public int VehicleId { get; set; }
+
+    public string? VehicleType { get; set; }
+
+    public byte? MaxPassengers { get; set; }
+
+    public byte? MaxLuggage { get; set; }
+
+    public byte? ChildSeats { get; set; }
+
+    public int? BaseFare { get; set; }
+
+    public string? ImageTitle { get; set; } = null!;
+
+    public string? ImageUrl { get; set; }
+
+    public string? Description { get; set; }
+
+
+
+    //public virtual ICollection<Vehicle> DriverShiftSchedules { get; set; } = new List<Vehicle>();
+}
