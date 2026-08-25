8fc530d62f1c8083ae56618822bc95751127ff33
jjason4608 <jjason71311@gmail.com>
2026-08-26 01:24:41 +0800

    加入常用地址頁面cshtml、cs、範例model、js

---
+++ Controllers/SavedAddressController.cs
@@ -0,0 +1,52 @@
+﻿using finalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace finalProject.Controllers
+{
+    public class SavedAddressController : Controller
+    {
+        private static List<SavedAddressViewModel> _addresses = new();
+
+        public IActionResult Index()
+        {
+            return View(_addresses);
+        }
+
+        [HttpPost]
+        public IActionResult Create(
+            string addressName,
+            string formattedAddress,
+            decimal latitude,
+            decimal longitude,
+            string placeId)
+        {
+            var newAddress = new SavedAddressViewModel
+            {
+                SavedAddressId = _addresses.Count + 1,
+                AddressName = addressName,
+                FormattedAddress = formattedAddress,
+                Latitude = latitude,
+                Longitude = longitude,
+                PlaceId = placeId
+            };
+
+            _addresses.Add(newAddress);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int savedAddressId)
+        {
+            var target = _addresses
+                .FirstOrDefault(x => x.SavedAddressId == savedAddressId);
+
+            if (target != null)
+            {
+                _addresses.Remove(target);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Models/SavedAddressViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace finalProject.Models
+{
+    public class SavedAddressViewModel
+    {
+        public int SavedAddressId { get; set; }
+
+        public string AddressName { get; set; } = string.Empty;
+
+        public string FormattedAddress { get; set; } = string.Empty;
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public string PlaceId { get; set; } = string.Empty;
+    }
+}
