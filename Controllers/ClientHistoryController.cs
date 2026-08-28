c0e0160bd755a59e639f55e8e12850b3997391cb
lance0913 <dickksss12341@gmail.com>
2026-08-28 09:21:27 +0800

    Controller先連接到資料庫，後續還沒處理

--- Controllers/ClientHistoryController.cs
+++ Controllers/ClientHistoryController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject.Controllers
 {
     public class ClientHistoryController : Controller
     {
+        private readonly RideHailingDbContext _context;
+
+        public ClientHistoryController(RideHailingDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult ClientHistory()
         {
             return View();
         }
+
+        // 待處理
+        //[HttpGet]
+        //public IActionResult GetOrders()
+        //{
+        //    var orders = _context.Trip
+        //        .Select(o => new
+        //        {
+        //            orderNumber = o.OrderNumber,
+        //            status = o.Status,
+        //            departureTime = o.DepartureTime,
+        //            pickupLocation = o.PickupLocation,
+        //            dropoffLocation = o.DropoffLocation,
+
+        //            licensePlate = o.LicensePlate,
+        //            carType = o.CarType,
+
+        //            passengerCount = o.PassengerCount,
+        //            luggageCount = o.LuggageCount,
+
+        //            price = o.Price,
+        //            estimatedMinutes = o.EstimatedMinutes,
+
+        //            passengerName = o.PassengerName,
+        //            passengerPhone = o.PassengerPhone,
+        //            remark = o.Remark
+        //        })
+        //        .ToList();
+
+        //    return Json(orders);
+        //}
+
+
+
+
     }
 }
