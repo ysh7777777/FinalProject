bcf0e768247b0e2632160dd62acdb83326646cee
lance0913 <dickksss12341@gmail.com>
2026-08-30 15:50:52 +0800

    修正Program的Pattern(.WithStaticAssets();的問題尚未處理)

--- Program.cs
+++ Program.cs
@@ -49,9 +49,9 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Booking}/{action=BookingPage}/{id?}")
+    //pattern: "{controller=Booking}/{action=BookingPage}/{id?}");
     //pattern: "{controller=Members}/{action=Login}/{id?}")
-    pattern: "{controller=ClientHistory}/{action=ClientHistory}/{id?}")
+    pattern: "{controller=ClientHistory}/{action=ClientHistory}/{id?}");
     //pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
