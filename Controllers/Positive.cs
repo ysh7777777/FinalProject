a7376d661e9d103b2bad969cd90a96f2ef0181fa
Kerstinyu009 <chouyu10811701@gmail.com>
2026-09-07 17:06:07 +0800

    新增乘客評分頁面、PForm.css、網路ison資源

---
+++ Controllers/Positive.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class Positive : Controller
+    {
+        public IActionResult PForm()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -49,8 +49,8 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    //pattern: "{controller=Members}/{action=Login}/{id?}")
+    //pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Positive}/{action=PForm}/{id?}")
     //pattern: "{controller=Booking}/{action=BookingPage}/{id?}")
     // pattern: "{controller=ClientHistory_New}/{action=ClientHistory_New}/{id?}")
     .WithStaticAssets();
